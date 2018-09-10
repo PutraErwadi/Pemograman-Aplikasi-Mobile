5aa3bfeef9989cf45f0c991d8443398a70cbf394
PutraErwadi <43114147+PutraErwadi@users.noreply.github.com>
2018-09-10 14:59:15 +0700

    Add files via upload

---
+++ tugas 1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int jumlah =4;
+        // TODO code application logic here
+        for (int i=0;i<jumlah;i++){
+            for(int j=0;j<=i;j++){
+            System.out.print("*");
+            }
+            System.out.print("\n");
+            System.out.print("\n");
+        }
+    }
+    
+        
+        
+
+}
